a163c6fb7b4f84ab60554441cb5fa29dbc0b4811
Tamer Osman <tamer4osman@gmail.com>
2018-07-27 12:31:20 +0200

    Create UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,26 @@
+public class UserInput {
+
+    public static class TextInput {
+        String value ="";
+        public void add(char c){
+            this.value = this.value+c;
+        }
+        public  String getValue(){
+            return this.value;
+        }
+    }
+
+    public static class NumericInput extends TextInput{
+        public void add(char c){
+            if (c>='0'&&c<='9')this.value = this.value+c;
+        }
+    }
+
+    public static void main(String[] args) {
+        TextInput input = new NumericInput();
+        input.add('1');
+        input.add('a');
+        input.add('0');
+        System.out.println(input.getValue());
+    }
+}
